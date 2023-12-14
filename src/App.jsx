df7e9ce9f6bed5a859879ac89d13b810296b62f0
Aung Kyaw Wai Htun <105596104+akwhtun@users.noreply.github.com>
2023-12-14 07:59:33 +0630

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="App bg-blue-500 text-emerald-400 p-4">
+      <h1 className="text-4xl font-bold">Hello React</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/*.html"],
+
+  theme: {
+    extend: {}, // You can add theme customizations here
+  },
+  plugins: [], // You can add plugins here if needed
+};
