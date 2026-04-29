0d4605587f5a07537d05d1c63347f1bee81041c0
akwhtun <akwhtun@gmail.com>
2026-04-29 21:03:37 +0630

    fixed error

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import AUNG_KYAW_WAI_HTUN_Resume from "../download/AUNG-KYAW-WAI-HTUN-Resume.pdf";
+import AUNG_KYAW_WAI_HTUN_Resume from "../download/AUNG_KYAW_WAI_HTUN_Resume.pdf";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleArrowDown } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
